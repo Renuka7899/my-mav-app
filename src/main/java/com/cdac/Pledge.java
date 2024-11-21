f5bd2dcc55f235b540041cc90a0c3332e0c8949b
Your Name <you@example.com>
2024-11-21 16:09:24 +0530

    adding maven project

---
+++ src/main/java/com/cdac/Pledge.java
@@ -0,0 +1,11 @@
+package com.cdac;
+
+public class Pledge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("India is my country.");
+        System.out.println("All Indians are my brothers and sisters.");
+	}
+
+}
